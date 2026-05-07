5bd4518a5c91b0d7d5fb4e98b47ed3ef7fd36ec1
Radek Novotny <rano@ciklum.com>
2026-05-07 12:33:42 +0200

    One more missing include in ex02

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 15:29:27 by rnovotny          #+#    #+#             */
-/*   Updated: 2026/05/06 19:49:26 by rnovotny         ###   ########.fr       */
+/*   Updated: 2026/05/07 12:33:12 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <iostream>
 # include <ctime>
 # include <iomanip>
+# include <cstdlib>
 
 // Uncomment the line below to enable comparison counting
 // #define COUNT_COMPARISONS
